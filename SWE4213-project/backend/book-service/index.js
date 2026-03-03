bb8b29781e13530a41cd2404a78d793c54576ba9
madisonb2 <mbrown31@unb.ca>
2026-03-03 18:46:35 -0400

    Added prisma to book-service

--- SWE4213-project/backend/book-service/index.js
+++ SWE4213-project/backend/book-service/index.js
@@ -1,19 +1,12 @@
 const express = require('express');
-const { Pool } = require('pg');
+const { PrismaClient } = require('./generated/client'); 
+const prisma = new PrismaClient();
+
 
 const app = express();
 app.use(express.json());
 
 const PORT = 3002;
-
-// PostgreSQL connection pool
-const pool = new Pool({
-  host: process.env.DB_HOST || 'book-db',
-  port: parseInt(process.env.DB_PORT || '5432'),
-  database: process.env.DB_NAME || 'bookdb',
-  user: process.env.DB_USER || 'postgres',
-  password: process.env.DB_PASSWORD || 'postgres',
-});
 
 app.get('/health', async (req, res) => {
   res.status(200).json({ message: 'Book service is running' });
@@ -21,8 +14,8 @@ app.get('/health', async (req, res) => {
 
 app.get('/books', async (req, res) => {
   try {
-    const result = await pool.query('SELECT * FROM books ORDER BY book_id');
-    res.json(result.rows);
+    const result = await prisma.books.findMany();
+    res.json(result);
   } catch (err) {
     console.error('Error fetching books:', err);
     res.status(500).json({ error: 'Internal server error' });
@@ -33,11 +26,13 @@ app.get('/books/:id', async (req, res) => {
   const { id } = req.params;
 
   try {
-    const result = await pool.query('SELECT * FROM books WHERE book_id = $1', [id]);
-    if (result.rows.length === 0) {
+    const result = await prisma.books.findFirst({
+      where: { book_id: Number(id)},
+    });
+    if (!result) {
       return res.status(404).json({ error: 'Book not found' });
     }
-    res.json(result.rows[0]);
+    res.json(result);
   } catch (err) {
     console.error('Error fetching book:', err);
     res.status(500).json({ error: 'Internal server error' });
@@ -52,11 +47,16 @@ app.post('/books', async (req, res) => {
   }
 
   try {
-    const result = await pool.query(
-      'INSERT INTO books (title, author, genre, summary, cover_url) VALUES ($1, $2, $3, $4, $5) RETURNING *',
-      [title, author, genre, summary, cover_url]
-    );
-    res.status(200).json(result.rows[0]);
+    const result = await prisma.books.create({
+      data: {
+        title: title,
+        author: author,
+        genre: genre,
+        summary: summary,
+        cover_url: cover_url,
+      },
+    });
+    res.status(200).json(result);
   } catch (err) {
     console.error('Error creating book:', err);
     res.status(500).json({ error: 'Internal server error' });
@@ -67,9 +67,9 @@ app.delete('/books/:id', async (req, res) => {
   const { id } = req.params;
 
   try {
-        const result = await pool.query(
-            "DELETE FROM books WHERE book_id = $1", [id]
-        );
+        const result = await prisma.books.delete({
+          where: {book_id: Number(id)}
+        });
         res.status(200).json({ message: "Book successfully deleted" })
     }
     catch (err) {
--- SWE4213-project/backend/book-service/init.sql
+++ SWE4213-project/backend/book-service/init.sql
@@ -7,28 +7,7 @@ CREATE TABLE IF NOT EXISTS books (
     cover_url TEXT
 );
 
-CREATE TABLE IF NOT EXISTS user_books (
-    user_book_id SERIAL PRIMARY KEY,
-    user_id INT NOT NULL,
-    book_id INT NOT NULL REFERENCES BOOKS(book_id),
-    have_read BOOLEAN
-);
-
-CREATE TABLE IF NOT EXISTS progress (
-    progress_id SERIAL PRIMARY KEY,
-    pages_read INT NOT NULL,
-    user_id INT NOT NULL,
-    book_id INT NOT NULL REFERENCES BOOKS(book_id)
-);
-
 INSERT INTO books (title, author, genre, summary, cover_url) VALUES
     ('The Hunger Games', 'Suzanne Collins', 'Science fiction', 'The Hunger Games follows 16-year-old Katniss Everdeen, a girl from District 12 who volunteers for the 74th Hunger Games in place of her younger sister Primrose Everdeen.', 'https://upload.wikimedia.org/wikipedia/en/d/dc/The_Hunger_Games.jpg'),
     ('Romeo and Juliet', 'William Shakespeare', 'Tragedy', 'Tragedy written by William Shakespeare about the romance between two young Italians from feuding families', 'https://m.media-amazon.com/images/S/compressed.photo.goodreads.com/books/1629680008i/18135.jpg');
 
-INSERT INTO user_books (user_id, book_id, have_read) VALUES
-    (1, 1, TRUE),
-    (1, 2, FALSE);
-    
-INSERT INTO progress (pages_read, user_id, book_id) VALUES
-    (100, 2, 1),
-    (2, 1, 2);
